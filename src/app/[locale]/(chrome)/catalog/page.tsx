f73e9eb1bfa8ea08b4c6b8186ec1a17286d558de
Artur-Kogay <9571artur9571@gmail.com>
2026-06-30 13:26:47 +0600

    fix: grid for products list on the catalog page

--- src/app/[locale]/(chrome)/catalog/page.tsx
+++ src/app/[locale]/(chrome)/catalog/page.tsx
@@ -1,16 +1,32 @@
-import { getTranslations } from 'next-intl/server';
+'use client';
 
-import { ProductsList } from '@/widgets/productsList';
+import { useAtomValue } from 'jotai';
+import { useTranslations } from 'next-intl';
+
+import { ProductCard, productsAtom } from '@/entities/product';
 
 import styles from './CatalogPage.module.scss';
 
-async function ProductsPage() {
-  const t = await getTranslations();
+function ProductsPage() {
+  const t = useTranslations();
+  const products = useAtomValue(productsAtom);
 
   return (
     <div className={styles.root}>
       <h1 className={styles.title}>{t('catalogPageTitle')}</h1>
-      <ProductsList />
+      <section className={styles.productsList}>
+        {products.map((item) => (
+          <ProductCard
+            key={item.id}
+            slug={item.slug}
+            title={item.title}
+            image={item.image}
+            price={item.price}
+            new={item.new}
+            author={item.author}
+          />
+        ))}
+      </section>
     </div>
   );
 }
