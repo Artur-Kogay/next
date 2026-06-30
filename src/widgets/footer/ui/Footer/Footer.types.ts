2e7feb96a6153c775789e3d7debb937e56c295a9
Artur-Kogay <9571artur9571@gmail.com>
2026-06-30 18:11:35 +0600

    feat: edit footer

--- src/widgets/footer/ui/Footer/Footer.types.ts
+++ src/widgets/footer/ui/Footer/Footer.types.ts
@@ -11,38 +11,6 @@ export interface FooterGroup {
 }
 
 export const NAV_GROUPS: readonly FooterGroup[] = [
-  // {
-  //   title: 'aboutCompany',
-  //   links: [
-  //     {
-  //       href: {
-  //         kg: '/content/public-offer-kassir',
-  //         uz: '/about/public_offer',
-  //         tj: '/about/public_offer',
-  //         ru: '/about/public_offer',
-  //       },
-  //       labelKey: 'public',
-  //     },
-  //     {
-  //       href: {
-  //         kg: '/content/for-organizators-kassir',
-  //         uz: '/about/organizations',
-  //         tj: '/about/organizations',
-  //         ru: '/about/organizations',
-  //       },
-  //       labelKey: 'organizeshion',
-  //     },
-  //     {
-  //       href: {
-  //         kg: '/about/logotips',
-  //         uz: '/about/logotips',
-  //         tj: '/about/logotips',
-  //         ru: '/about/logotips',
-  //       },
-  //       labelKey: 'logo',
-  //     },
-  //   ],
-  // },
   {
     title: 'navigation',
     links: [
@@ -73,54 +41,56 @@ export const NAV_GROUPS: readonly FooterGroup[] = [
         },
         labelKey: 'catalog',
       },
-      // {
-      //   href: {
-      //     kg: '/content/buy-rules-kassir',
-      //     uz: '/about/howtobuy',
-      //     tj: '/about/howtobuy',
-      //     ru: '/about/howtobuy',
-      //   },
-      //   labelKey: 'purchaseinstructions',
-      // },
-      // {
-      //   href: {
-      //     kg: '/content/faq-kassir',
-      //     uz: '/about/faq',
-      //     tj: '/about/faq',
-      //     ru: '/about/faq',
-      //   },
-      //   labelKey: 'questionfooter',
-      // },
-      // {
-      //   href: { kg: '/content/ticket-usage-rules' },
-      //   labelKey: 'ticketsRules',
-      // },
-      // {
-      //   href: { kg: '/content/refund-rules-kassir' },
-      //   labelKey: 'roots',
-      // },
-      // {
-      //   href: {
-      //     kg: '/content/terms-and-conditions-kassir',
-      //     uz: '/about/termsconditions',
-      //     tj: '/about/termsconditions',
-      //     ru: '/about/termsconditions',
-      //   },
-      //   labelKey: 'foydalanishShartlari',
-      // },
-      // {
-      //   href: {
-      //     kg: '/content/online-tickets-kassir',
-      //     uz: '/about/etickets',
-      //     tj: '/about/etickets',
-      //     ru: '/about/etickets',
-      //   },
-      //   labelKey: 'etickets',
-      // },
-      // {
-      //   href: { kg: '/scun' },
-      //   labelKey: 'checkTicket',
-      // },
+    ],
+  },
+  {
+    title: 'rules',
+    links: [
+      {
+        href: {
+          kg: '/',
+          uz: '/',
+          tj: '/',
+          ru: '/',
+        },
+        labelKey: 'userAgreement',
+      },
+      {
+        href: {
+          kg: '/',
+          uz: '/',
+          tj: '/',
+          ru: '/',
+        },
+        labelKey: 'returnPolicy',
+      },
+      {
+        href: {
+          kg: '/',
+          uz: '/',
+          tj: '/',
+          ru: '/',
+        },
+        labelKey: 'delivery',
+      },
+      {
+        href: {
+          kg: '/',
+          uz: '/',
+          tj: '/',
+          ru: '/',
+        },
+        labelKey: 'forHolders',
+      },
+      {
+        href: {
+          kg: '/',
+          uz: '/',
+          tj: '/',
+          ru: '/',
+        },
+        labelKey: 'privacyPolicy',
+      },
     ],
   },
 ];
